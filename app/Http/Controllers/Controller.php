f6ebb0c42897c53ac1b9a8236498e5b9c7393f3f
khrnnisak <khrnnisak07@gmail.com>
2022-03-13 20:01:18 +0700

    penambahan view dan controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class PageController extends Controller
+class Controller extends BaseController
 {
-    public function index(){
-        return view('konten');
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
